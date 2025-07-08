895fe80a5795725ac595b4fcacc904fc91041f92
BRUNDAVANAM SUREKHA <122956099+BRUNDAVANAMSUREKHA@users.noreply.github.com>
2025-07-08 15:22:39 +0530

    feat: added home.php with landing page layout

---
+++ pages/home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Imports in India</title>
+  <link rel="stylesheet" href="../public/css/home.css"> <!-- External CSS File -->
+  <link rel="icon" type="image/png" href="/import/public/images/favicon/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/import/public/images/favicon/favicon.svg" />
+  <link rel="shortcut icon" href="/import/public/images/favicon/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/import/public/images/favicon/apple-touch-icon.png" />
+  <link rel="manifest" href="/import/public/images/favicon/site.webmanifest" />
+  <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+</head>
+<body>
+
+  <div class="hero-section">
+    <div class="info-container">
+      <h1>IMPORTS IN INDIA</h1>
+      <p><b>India is one of the largest importers of goods in the world, bringing in essential resources, technology, and consumer 
+        products. "Our Import Database provides comprehensive and up-to-date information on goods imported into India, including product 
+        categories, trade volumes, and origin countries. It serves as a valuable resource for businesses, policymakers, and 
+        researchers to analyze trends, ensure compliance, and optimize trade strategies."</b></p>
+    </div>
+  </div>
+  
+  <div class="cards-section">
+    <a href="../pages/import.php" class="card">
+      <img src="../public/images/home/card1.jpeg" alt="Import 1">
+      <div class="card-overlay">
+        <p>Why India Needs Imports?</p>
+      </div>
+    </a>
+    <a href="../pages/states.php" class="card">
+      <img src="../public/images/home/card2.jpg" alt="Import 2">
+      <div class="card-overlay">
+        <p>Imports Based on States</p>
+      </div>
+    </a>
+    <a href="../pages/categories.php" class="card">
+      <img src="../public/images/home/card3.jpg" alt="Import 3">
+      <div class="card-overlay">
+        <p>Imports Based on Category</p>
+      </div>
+    </a>
+  </div>
+
+  <script src="../public/js/home.js"></script> <!-- External JS File -->
+</body>
+</html>
